d99c248db72fd3292485358e7fbcceae4e67c1fa
CyberMermaid <45833464+CyberMermaid@users.noreply.github.com>
2023-12-17 20:58:46 -0500

    Update Pokecard.js & Pokedex.js

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Pokedex } from './Pokedex.js';
+
+const App = () => {
+  return (
+    <div>
+      <Pokedex/>
+    </div>
+  );
+}
+
+
+export default App;
+
+ReactDOM.render(<App/>, document.getElementById("root"))
